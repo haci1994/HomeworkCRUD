d08719e8bd9c5fecdf2accf7896b95e7825ac790
haci1994 <mmzadj@gmail.com>
2025-09-04 12:45:29 +0400

    Added FooterViewComponent

---
+++ HomeworkCRUD/ViewComponents/FooterViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using HomeworkCRUD.DataContext;
+using HomeworkCRUD.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HomeworkCRUD.ViewComponents
+{
+    public class FooterViewComponent : ViewComponent
+    {
+        private readonly AppDbContext _dbContext;
+
+        public FooterViewComponent(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var socials = await _dbContext.Socials.ToListAsync();
+            var headerElement = await _dbContext.HeaderElements.FirstOrDefaultAsync();
+
+            FooterViewModel model = new FooterViewModel
+            {
+                Socials = socials,
+                HeaderElement = headerElement
+            };
+
+            return View(model);
+        }
+    }
+}
